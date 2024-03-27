0241d8153152c9fd095d7fc887c1dd56e98a8192
Danii9790 <162893232+Danii9790@users.noreply.github.com>
2024-03-27 10:09:40 +0500

    Atm Machine

---
+++ main.js
@@ -0,0 +1,56 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalance = "20000";
+let mypin = "4321";
+let pinanswer = await inquirer.prompt([
+    {
+        message: "Enter your pin",
+        name: "pin",
+        type: "number"
+    }
+]);
+if (pinanswer.pin == mypin) {
+    console.log("Welcome to ATM");
+    let option = await inquirer.prompt([
+        {
+            message: "Select your option",
+            name: "option",
+            type: "list",
+            choices: ["Balance Inquiry", "Withdraw", "Deposit", "Exit"]
+        }
+    ]);
+    if (option.option == "Balance Inquiry") {
+        console.log(`Your balance is ${mybalance}`);
+    }
+    else if (option.option == "Withdraw") {
+        let withdrawanswer = await inquirer.prompt([
+            {
+                message: "Enter the amount",
+                name: "amount",
+                type: "number"
+            }
+        ]);
+        if (withdrawanswer.amount <= mybalance) {
+            mybalance = (+mybalance - +withdrawanswer.amount).toString();
+            console.log(`Your balance is ${mybalance}`);
+        }
+        else {
+            console.log("Insufficient balance");
+        }
+    }
+    else if (option.option == "Deposit") {
+        let depositanswer = await inquirer.prompt([
+            {
+                message: "Enter the amount",
+                name: "amount",
+                type: "number"
+            }
+        ]);
+        mybalance = (+mybalance + +depositanswer.amount).toString();
+        console.log(`Your balance is ${mybalance}`);
+    }
+    if (option.option == "Exit") {
+        console.log("Thank you for using ATM");
+    }
+}
+;
